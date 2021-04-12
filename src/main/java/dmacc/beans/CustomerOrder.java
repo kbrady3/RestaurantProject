a20f6df75efeb017cdaf596d5f2a851a7c7d4b99
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-04-11 22:03:08 -0500

    Changed some things in the beans to try to get it working
    Seems like it's on the right track, but not sure

--- src/main/java/dmacc/beans/CustomerOrder.java
+++ src/main/java/dmacc/beans/CustomerOrder.java
@@ -2,22 +2,27 @@ package dmacc.beans;
 
 import java.time.LocalDate;
 
+import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Abutalib Hasan - amhasan
  * 202101 CIS171 12928
  * Apr 2, 2021
  */
-@Entity
+@Embeddable
+//@Entity
 public class CustomerOrder {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private LocalDate date;
+	@Autowired
 	private Customer customer;
 	
 
