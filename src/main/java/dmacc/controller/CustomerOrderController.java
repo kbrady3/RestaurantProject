747ea4fd3fa9cea87cb39fa3af3f5c8fe0a43095
jrbrannen <71732832+jrbrannen@users.noreply.github.com>
2021-04-28 20:57:27 -0500

    Revert "merge"

--- src/main/java/dmacc/controller/CustomerOrderController.java
+++ src/main/java/dmacc/controller/CustomerOrderController.java
@@ -1,8 +1,6 @@
 
 package dmacc.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,12 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import dmacc.beans.Customer;
 import dmacc.beans.CustomerOrder;
-import dmacc.beans.MenuItems;
 import dmacc.repository.CustomerOrderRepository;
-import dmacc.repository.MenuItemsRepoitory;
 
 /**
  * @author Jeremy Brannen - jrbrannen
@@ -28,19 +22,13 @@ public class CustomerOrderController {
 	@Autowired
 	CustomerOrderRepository repo;
 	
-	@Autowired
-	MenuItemsRepoitory r;
-	
 	// display an order
-	@GetMapping({"/displayCustomerOrder"})
+	@GetMapping({"update/displayCustomerOrder"})
 	public String viewCustomerOrder(Model model) {
-		
-/**
 		if(repo.findAll().isEmpty()) {
 			return addCustomerOrder(model);
 		}
-**/
-		model.addAttribute("orderDetails", repo.findAll());
+		model.addAttribute("CustomerOrder", repo.findAll());
 		return "displayCustomerOrder";
 	}
 	
@@ -48,36 +36,28 @@ public class CustomerOrderController {
 	@GetMapping("/addCustomerOrder")
 	public String addCustomerOrder(Model model) {
 			CustomerOrder o = new CustomerOrder();
-			List<MenuItems> itemsList = r.findAll();
 			model.addAttribute("newCustomerOrder", o);
-			model.addAttribute("itemsList", itemsList);
-			return "CustomerOrder";
+			return "displayCustomerOrder";
 	}
-
-	@PostMapping("/addCustomerOrder")
-	public String addCustomerOrder(@ModelAttribute CustomerOrder o, Model model) {
-		repo.save(o);
-		return viewCustomerOrder(model);
-		//return "displayCustomerOrder";
-	}
-
+	
 	// edits order pull order from database by id
 	@GetMapping("/editOrder/{id}")
 	public String editCustomerOrder(@PathVariable("id") long id, Model model) {
 		CustomerOrder o = repo.findById(id).orElse(null);
 		model.addAttribute("newOrder", o);
-		return "CustomerOrder";
+		return "input";
 	}
-
+	
 	// updates a current order
-	@PostMapping("/updateOrder/{id}")
-	public String updateCustomerOrder(@ModelAttribute CustomerOrder o, Model model) {
+	@PostMapping("/createCustomerOrder/{id}")
+	public String updateOrder(@ModelAttribute CustomerOrder o, Model model) {
 		repo.save(o);
 		return viewCustomerOrder(model);
-	}	
+	}
+	
 	// deletes current order id
 	@GetMapping("/deleteCustomerOrder/{id}")
-	public String deleteCustomerOrder(@PathVariable("id") long id, Model model) {
+	public String deleteOrder(@PathVariable("id") long id, Model model) {
 		CustomerOrder o = repo.findById(id).orElse(null);
 		repo.delete(o);
 		return viewCustomerOrder(model);
